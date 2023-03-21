31370bc88abd21db5b74d206e8d2cd6dd448fa15
modii <modiibeats@gmail.com>
2023-03-21 03:46:37 +0000

    Revert "reset"
    This reverts commit 92b04854f11b2a9bea1ab4414ffbaa3e7acb0706.

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
