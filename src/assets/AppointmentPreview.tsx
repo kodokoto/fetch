0dab19df03626a8afdab3688c93eb32decc5d569
hazalDK <94861070+hazalDK@users.noreply.github.com>
2023-03-25 13:58:20 +0000

    Made files

---
+++ src/assets/AppointmentPreview.tsx
@@ -0,0 +1,13 @@
+import { ImageBackground } from 'react-native';
+import { Box } from 'native-base';
+//Import pages from screens
+// import Appointment from '../screens/Appointment';
+import React from "react";
+
+// const Appointment = createBottomTabNavigator();
+
+export default function AppointmentsPreview() {
+    return <Box>
+        
+    </Box>
+}
