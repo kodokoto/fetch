a4252ce3487c1e65cdf575d3a4a9fd5b155b25fb
Samiha <samihakamal.7664@gmail.com>
2023-03-29 00:42:39 +0100

    Basic filter searching

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Filter from '../components/Filter';
+import { NativeBaseProvider } from 'native-base';
 
 export default function Home() {
   return (
-    <View>
-      <Text className="text-red-500">Test</Text>
-    </View>
+      <NativeBaseProvider>
+        <View>
+          <Text className="text-red-500">Test</Text> 
+        </View>
+        <Filter />
+      </NativeBaseProvider>
   )
 }
