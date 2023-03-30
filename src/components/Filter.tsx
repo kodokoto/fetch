e1e17c15a9070069a91feab0273e9c345544b22a
Samiha <samihakamal.7664@gmail.com>
2023-03-31 00:34:32 +0100

    Cleaned up and removed import on homepage

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,14 +1,12 @@
-import { View, Text } from 'react-native'
 import React from "react";
-import { Box, Select, Button, Input, CheckIcon, Center, Radio, Heading,  NativeBaseProvider } from "native-base";
+import { Box, Select, FormControl, Text, Button, Input, HStack, CheckIcon, Radio, NativeBaseProvider } from "native-base";
 import { useNavigation } from '@react-navigation/native';
 
 //Rating
 const Rating = () => {
   const [service, setService] = React.useState("");
-  return <Center>
-      <Box maxW="300">
-        <Select selectedValue={service} minWidth="100" accessibilityLabel="Choose rating" placeholder="Rating" _selectedItem={{
+  return <Box maxW="full">
+        <Select selectedValue={service} minWidth="full" accessibilityLabel="Choose rating" placeholder="Rating" _selectedItem={{
         bg: "teal.600",
         endIcon: <CheckIcon size="5" />
       }} mt={1} onValueChange={itemValue => setService(itemValue)}>
@@ -19,15 +17,14 @@ const Rating = () => {
           <Select.Item label="5" value="five" />
         </Select>
       </Box>
-    </Center>;
+    
 };
 
 //Dropdown for service
 const Service = () => {
   const [service, setService] = React.useState("");
-  return <Center>
-      <Box maxW="300">
-        <Select selectedValue={service} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
+  return <Box maxW="full">
+        <Select selectedValue={service} minWidth="full" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
         bg: "teal.600",
         endIcon: <CheckIcon size="5" />
       }} mt={1} onValueChange={itemValue => setService(itemValue)}>
@@ -36,21 +33,19 @@ const Service = () => {
           <Select.Item label="House sitting" value="hs" />
         </Select>
       </Box>
-    </Center>;
+    
 };
 
 //Open for recurring visit
 const RadioButton = () => {
   const [value, setValue] = React.useState("Yes");
-  return <Radio.Group name="radioButtons" accessibilityLabel="Available for recurring visit" value={value} onChange={nextValue => {
-    setValue(nextValue);
-  }}>  
-      <Radio value="Yes" my={1}>
-        Yes
-      </Radio>
-      <Radio value="No" my={1}>
-        No
-      </Radio>
+  return  <Radio.Group name="radioButtons" accessibilityLabel="Available for recurring visit" value={value} onChange={nextValue => { setValue(nextValue); }}>
+    <HStack space={5}>
+     <Radio value="Yes" my={1}>Yes</Radio>
+      <Radio value="No" my={1}>No</Radio>
+     </HStack>
+   
+       
     </Radio.Group>;
 };
 
@@ -63,20 +58,26 @@ const RadioButton = () => {
 
     return (
       <NativeBaseProvider>
-        <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-          <Heading>Time available:</Heading>
-          <Input mx="3" placeholder="Start time" w="150" />
-          <Heading>to</Heading>
-          <Input mx="3" placeholder="End time" w="150" />
-          <Heading>Service</Heading>
+        <Box flex={1} bg="#fff" p={4}>
+          <FormControl.Label _text={{ bold: true }}>Time Available:</FormControl.Label>
+          <HStack space={1}>
+          <Input ml={0} mx="3" placeholder="Start time" w="150" keyboardType='numeric' />
+          <Text my={3}>to</Text>
+          <Input mx="3" placeholder="End time" w="150" keyboardType='numeric'/>
+          </HStack>
+          <FormControl.Label _text={{ bold: true }}>Service:</FormControl.Label>
           <Service />
-          <Heading>Open for recurring visit?</Heading>
+          <FormControl.Label _text={{ bold: true }}>Open for recurring visit:</FormControl.Label>
           <RadioButton />
-          <Heading>User rating:</Heading>
+          <FormControl.Label _text={{ bold: true }}>User rating:</FormControl.Label>
           <Rating />
-          <Heading>Proximity:</Heading>
-          <Input mx="3" placeholder="Distance" w="100" />
-          <Button onPress={handlePress}>Submit</Button>
+          <FormControl.Label _text={{ bold: true }}>Proximity:</FormControl.Label>
+          <HStack space={2}>
+            <Input my="1" placeholder="Distance" w='300' keyboardType='numeric' />
+            <Text my={4}>Miles</Text>
+          </HStack>
+          
+          <Button my={5} onPress={handlePress}>Submit</Button>
         </Box>
       </NativeBaseProvider>
   
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Filter from '../components/Filter';
-import { NativeBaseProvider } from 'native-base';
 
 export default function Home() {
   return (
-      <NativeBaseProvider>
-        <View>
-          <Text className="text-red-500">Test</Text> 
-        </View>
-        <Filter />
-      </NativeBaseProvider>
+    <View>
+      <Text className="text-red-500">Test</Text> 
+    </View>
+        
   )
 }
