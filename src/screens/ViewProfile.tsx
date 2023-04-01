137f7537d0073080202355539040cdfaa73cdf57
Kazeo <kkhmail1@gmail.com>
2023-04-01 14:56:30 +0100

    Update ViewProfile.tsx

--- src/screens/ViewProfile.tsx
+++ src/screens/ViewProfile.tsx
@@ -1,24 +1,36 @@
-import { Image, View, Text } from 'react-native'
+import { Image, View, Text} from 'react-native'
+import { Button, Box, NativeBaseProvider } from 'native-base'
 import React from 'react'
 import SitterProfileLocation from '../components/SitterProfileLocation'
 import ProfileRating from '../components/ProfileRating'
 
 export default function ViewProfile() {
   return (
-    <View>
-        <Image 
-            style={{
-                width: 100, 
-                height: 100,
-                borderRadius: 50
-            }}
-            source={require('../assets/pfp.png')}
-        />
-        {/* PLaceholder components */}
+    <NativeBaseProvider>
+      <View>
+        <Image
+          className='w-28 h-28 rounded-full'
+          source={require('../assets/pfp.png')} />
+        {/* Placeholder components */}
         <Text className="text-orange">Leonard Lungu</Text>
         <Text>Pet Sitter</Text>
-        <ProfileRating/>
-        <SitterProfileLocation/>
-    </View>
+        <ProfileRating />
+        <SitterProfileLocation />
+
+        <View className='flex-row'>
+          <Button className='border-gray-500 border-2 bg-white'>
+            <Text className="text-black-500">Book</Text>
+          </Button>
+          <Button className='border-gray-500 border-2 bg-white'>
+            <Text className="text-black-500">Message</Text>
+          </Button>
+          <Button className='border-gray-500 border-2 bg-white'>
+            <Text className="text-red-500">Report</Text>
+          </Button>
+        </View>
+
+        
+      </View>
+    </NativeBaseProvider>
   )
 }
