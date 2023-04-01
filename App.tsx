8d2ca25c4ea78fd48324edb57a2ce7618d31feef
Kazeo <kkhmail1@gmail.com>
2023-04-01 18:47:35 +0100

    implemented all components for screen. styling not done yet

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Tabs from './src/components/Tab';
-
+import { NativeBaseProvider } from 'native-base';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
--- src/components/SitterProfileLocation.tsx
+++ src/components/SitterProfileLocation.tsx
@@ -1,12 +1,21 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Text, View } from 'react-native'
+import { Button, Box, NativeBaseProvider } from 'native-base'
+import React from 'react'
 
-export default function SitterProfileLocation() {
+interface Props {
+  location: string
+}
+const distance = 5;
 
+export default function SitterProfileLocation(props: Props) {
   return (
     <View>
-        <Text className="text-black-500">London</Text>
-        <Text className="text-black-500">5 miles away</Text>
+      <Text className="text-xl">Location</Text>
+      <Box className="border-2 flex-row">
+        <Text className="text-xl">{props.location}</Text>
+        <Text className="text-xl">{distance} miles away</Text>
+      </Box>
+      
     </View>
   )
 }
