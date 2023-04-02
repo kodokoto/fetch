aacbbc030a400f9e664458245f6e559191dac415
hazalDK <94861070+hazalDK@users.noreply.github.com>
2023-04-02 13:15:48 +0100

    Fixed navigation

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Tabs from './src/components/Tab';
 import { NativeBaseProvider } from 'native-base';
+import ViewProfile from './src/screens/ViewProfileSitter';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator>  
         <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />   
         <Stack.Screen name="Home" component={Home} />       
+        <Stack.Screen name="ViewProfileSitter" component={ViewProfile} />
            
       </Stack.Navigator>  
     </NavigationContainer>
--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,19 +1,20 @@
 import { View, Text } from 'react-native'
 import { Button, NativeBaseProvider } from 'native-base'
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import React from 'react'
 
 export default function Chat() {
-
+  const navigation = useNavigation();  
   return (
     <NativeBaseProvider>
       <View>
         <Text className="text-orange-500">Test</Text>
         {/* THIS IS A PLACEHOLDER FOR THE SEARCH RESULTS, PRESS ON THOSE MOFOS AND IT REDIRECTS YOU TO ViewProfielSitter.tsx */}
         <Button
-          onPress={() => navigation.navigate('ViewProfileSitter.tsx')}
+          onPress={() => navigation.navigate('ViewProfileSitter')}
         >
           <Text>Leonard Lungu</Text>
         </Button>
