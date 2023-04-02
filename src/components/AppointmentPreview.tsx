3b57869961b9f44f7f129499114d3efd438119d3
hazalDK <94861070+hazalDK@users.noreply.github.com>
2023-04-02 23:36:22 +0100

    used npx prettier

--- src/components/AppointmentPreview.tsx
+++ src/components/AppointmentPreview.tsx
@@ -1,56 +1,65 @@
 //Import @React-navigation/native
-import { useNavigation } from '@react-navigation/native';
-import { Image, Text } from 'react-native';
-import { Button, Box, FormControl, Avatar } from 'native-base';
+import { useNavigation } from '@react-navigation/native'
+import { Image, Text } from 'react-native'
+import { Button, Box, FormControl, Avatar } from 'native-base'
 import { Ionicons } from '@expo/vector-icons'
-import React from "react";
-
+import React from 'react'
 
 type AppointmentPreviewProps = {
-    id: number,
-    imageUrl: string;
-    sitterName: string;
-    appointment: string;
-    dateDescription: string;
-    timeDescription: string;
-    bookingFrequency: string;
+  id: number
+  imageUrl: string
+  sitterName: string
+  appointment: string
+  dateDescription: string
+  timeDescription: string
+  bookingFrequency: string
 }
 
 export default function AppointmentsPreview(props: AppointmentPreviewProps) {
-    const navigation = useNavigation();
+  const navigation = useNavigation()
 
-    const handlePress = () => {
-        const appointmentData = {
-          id: props.id,
-          imageUrl: props.imageUrl,
-          sitterName: props.sitterName,
-          appointment: props.appointment,
-          dateDescription: props.dateDescription,
-          timeDescription: props.timeDescription,
-          bookingFrequency: props.bookingFrequency
-        };
-        navigation.navigate('AppointmentDetails', { appointmentData });
-      };
+  const handlePress = () => {
+    const appointmentData = {
+      id: props.id,
+      imageUrl: props.imageUrl,
+      sitterName: props.sitterName,
+      appointment: props.appointment,
+      dateDescription: props.dateDescription,
+      timeDescription: props.timeDescription,
+      bookingFrequency: props.bookingFrequency,
+    }
+    navigation.navigate('AppointmentDetails', { appointmentData })
+  }
 
-    return(
-        <Button onPress={handlePress} className='m-auto rounded-2xl w-80 bg-[#4c8ab9] mt-6 h-30 hover:bg-[#4c8ab9]  border-solid border-transparent border-2'>
-            <Box className='bg-[#4c8ab9] rounded-2xl p-4 w-80 h-25 mb-2 flex-wrap flex-row justify-between'>
-                <Box className='flex-start' style={{ flexDirection: 'row' }}>
-                    <Avatar source={{uri: props.imageUrl}} className='w-12 h-12 md:w-48 md:h-auto float-left'></Avatar>
-                    <Box className='ml-4 float-left'>
-                        <Text className='font-bold text-lg'>{props.sitterName}</Text>
-                        <Text>{props.appointment}</Text>
-                        {/* <Text>{props.dateDescription}</Text> */}
-                    </Box>
-                </Box>
-                <Text className='flex-end'>{props.bookingFrequency}</Text>
-            </Box>
-            <Box className='ml-4 flex-wrap flex-row'>
-                <Ionicons size={24} className='flex-start' name="ios-calendar-outline"></Ionicons>
-                <Text className='mx-2 text-md'>{props.dateDescription}</Text>
-                <Ionicons size={24} name="ios-time-outline"></Ionicons>
-                <Text className='mx-2 text-md'>{props.timeDescription}</Text>
-            </Box>
-        </Button>
-    );
+  return (
+    <Button
+      onPress={handlePress}
+      className="m-auto rounded-2xl w-80 bg-[#4c8ab9] mt-6 h-30 hover:bg-[#4c8ab9]  border-solid border-transparent border-2"
+    >
+      <Box className="bg-[#4c8ab9] rounded-2xl p-4 w-80 h-25 mb-2 flex-wrap flex-row justify-between">
+        <Box className="flex-start" style={{ flexDirection: 'row' }}>
+          <Avatar
+            source={{ uri: props.imageUrl }}
+            className="w-12 h-12 md:w-48 md:h-auto float-left"
+          ></Avatar>
+          <Box className="ml-4 float-left">
+            <Text className="font-bold text-lg">{props.sitterName}</Text>
+            <Text>{props.appointment}</Text>
+            {/* <Text>{props.dateDescription}</Text> */}
+          </Box>
+        </Box>
+        <Text className="flex-end">{props.bookingFrequency}</Text>
+      </Box>
+      <Box className="ml-4 flex-wrap flex-row">
+        <Ionicons
+          size={24}
+          className="flex-start"
+          name="ios-calendar-outline"
+        ></Ionicons>
+        <Text className="mx-2 text-md">{props.dateDescription}</Text>
+        <Ionicons size={24} name="ios-time-outline"></Ionicons>
+        <Text className="mx-2 text-md">{props.timeDescription}</Text>
+      </Box>
+    </Button>
+  )
 }
--- src/screens/AppointmentDetails.tsx
+++ src/screens/AppointmentDetails.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
-import { useRoute } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native'
 import React from 'react'
-import AppDetail from '../components/AppDetail';
+import AppDetail from '../components/AppDetail'
 
 export const mockAppointmentData = [
   {
     id: 1,
-    imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
     sitterName: 'Joey',
     username: 'PetLover101',
     pet: 'Fufu',
@@ -15,11 +16,12 @@ export const mockAppointmentData = [
     dateDescription: 'Friday, 21 April',
     timeDescription: '1:00 - 4:00 PM',
     bookingFrequency: 'monthly',
-    mostRecentMessage: 'hello'
+    mostRecentMessage: 'hello',
   },
   {
     id: 2,
-    imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
     sitterName: 'Jessica',
     username: 'Jessica Bifford',
     pet: 'Mario',
@@ -28,25 +30,24 @@ export const mockAppointmentData = [
     dateDescription: 'Monday, 29 July',
     timeDescription: '11:00 - 12:00 AM',
     bookingFrequency: 'weekly',
-    mostRecentMessage: 'hi'
-  }
+    mostRecentMessage: 'hi',
+  },
 ]
 
-
 export default function AppointmentDetails() {
-  const route = useRoute();
-  const id = route.params.appointmentData.id;
-  let matchedAppointment = null;
-  for(const appointment of mockAppointmentData){
-    if(appointment.id === id){
-      matchedAppointment = appointment;
+  const route = useRoute()
+  const id = route.params.appointmentData.id
+  let matchedAppointment = null
+  for (const appointment of mockAppointmentData) {
+    if (appointment.id === id) {
+      matchedAppointment = appointment
     }
   }
 
   return (
     <View>
       {/* <Text className="text-2xl mx-auto mt-4">Appointment Details</Text> */}
-      <AppDetail {...matchedAppointment}/>
+      <AppDetail {...matchedAppointment} />
     </View>
   )
 }
