2567431e59a6f967e26cef91eea8f80d3c8e1391
hazalDK <94861070+hazalDK@users.noreply.github.com>
2023-04-09 18:52:54 +0100

    Update SearchResult.tsx

--- packages/app/components/SearchResult.tsx
+++ packages/app/components/SearchResult.tsx
@@ -8,7 +8,7 @@ export default function SearchResult(props) {
   const router = useRouter()
   const {date, service, availability, maxPrice, dateTime} = useSearchParams();
   console.log(props.searchResult)
-  const { data } = api.sitter.byId.useQuery(props.searchResult.userId, { enabled: !!props.searchResult.userId })
+  const { data } = api.sitter.byId.useQuery(props.searchResult.id, { enabled: !!props.searchResult.id })
   const petType = api.service.bySitterId.useQuery(props.searchResult.id).data
   console.log('Pet Type: ' + JSON.stringify(petType))
   return (
@@ -20,7 +20,7 @@ export default function SearchResult(props) {
     >
       <TouchableOpacity
         style={{ flexDirection: 'row' }}
-        className='m-auto rounded-2xl w-80 bg-slate-100 mt-6 h-32 border-solid border-[#4c8ab9] border-2'
+        className='m-auto rounded-2xl w-80 bg-slate-100 mt-6 h-24 border-solid border-[#4c8ab9] border-2'
         onPress={() =>
           router.push({
             pathname: `/booking/addBooking`,
@@ -35,8 +35,7 @@ export default function SearchResult(props) {
         }
       >
         {data ? (
-          <Avatar className="w-12 h-12 ml-4 mt-4 float-left" source={{ uri: data.imageUrl }}
-          />
+          <Avatar className="w-12 h-12 ml-4 mt-4 float-left" source={{ uri: data.imageUrl }}/>
         ) : null}
         <Box className="ml-4 float-left">
           <Text>{data ? data.name : null}</Text>
@@ -47,8 +46,9 @@ export default function SearchResult(props) {
         <Text
           style={{
             alignSelf: 'flex-start',
-            marginLeft: 'auto',
+            // marginLeft: 'auto',
           }}
+          className='ml-auto'
         >
           £{petType? petType.price : null}
         </Text>
