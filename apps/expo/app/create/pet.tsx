39d85c7c6a51793d87c592029bb03cb0b22a94f1
hazalDK <94861070+hazalDK@users.noreply.github.com>
2023-04-10 18:42:05 +0100

    styling done

--- apps/expo/app/create/pet.tsx
+++ apps/expo/app/create/pet.tsx
@@ -1,10 +1,11 @@
-import { Button, ScrollView, Select, FormControl, TextArea, CheckIcon, VStack } from "native-base";
+import { Button, ScrollView, Select, FormControl, TextArea, Box } from "native-base";
 import { api } from 'app/utils/trpc'
 import { Owner } from "db";
 import React from "react";
+import { useRouter } from 'expo-router'
 
 export default function petCreateForm(props: Owner) {
-
+    const router = useRouter();
     const mutation = api.pet.create.useMutation()
     const handleSubmit = () => {
         mutation.mutate({
@@ -25,7 +26,7 @@ export default function petCreateForm(props: Owner) {
     return (
         <ScrollView>
             <FormControl.Label>Name:</FormControl.Label>
-            <TextArea value={name} onChangeText={text => setName(text)} autoCompleteType={undefined}></TextArea>
+            <TextArea value={name} onChangeText={text => setName(text)} autoCompleteType={undefined} h={8}></TextArea>
             <FormControl.Label>PetType:</FormControl.Label>
             <Select 
                 selectedValue={SelectedPetType}
@@ -39,7 +40,11 @@ export default function petCreateForm(props: Owner) {
             </Select>
             <FormControl.Label>Description:</FormControl.Label>
             <TextArea value={description} onChangeText={text => setDescription(text)} autoCompleteType={undefined}></TextArea>
-            <Button onPress={handleSubmit}>Add Pet</Button>
+            <Box className="flex-wrap flex-row mt-2 mb-10">
+                <Button className="ml-auto rounded-2xl" onPress={handleSubmit}>Add Pet</Button>
+                <Button className="mr-auto ml-2 rounded-2xl">Cancel</Button>
+            </Box>
+            
         </ScrollView>
     )
 }
