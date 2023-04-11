9eb80bf7cf55cd4ae12a71ad21236684c61540f0
Apower11 <adampower45@hotmail.com>
2023-04-11 21:44:37 +0100

    minor edits

--- packages/app/components/ProfileCarousel.tsx
+++ packages/app/components/ProfileCarousel.tsx
@@ -3,46 +3,65 @@ import { Button } from 'native-base';
 import { Ionicons } from '@expo/vector-icons'
 import Carousel from 'react-native-reanimated-carousel';
 import { Image } from '@prisma/client'
-import {launchImageLibrary, MediaType} from 'react-native-image-picker';
-import { api } from '../utils/trpc';
-import { useAtom } from 'jotai'
-import { Profile, sessionAtom } from 'app/utils/storage'
-import * as ImagePicker from 'expo-image-picker';
-import AddPictureButton from './AddPictureButton';
+import * as ImagePicker from 'react-native-image-picker';
 
-export default function ProfileCarousel(props) {
-    let showAddPictureButton = false;
+export default function ProfileCarousel(images: Image[]) {
+    // var ImagePicker = require('react-native-image-picker');
     const width = Dimensions.get('window').width;
 
-    const [session, _] = useAtom(sessionAtom)
-
-    if(session.sitterId == props.sitterId){
-        showAddPictureButton = true;
-    }
-
+    const selectPhotoTapped = async () => {
+        console.log("Hello");
+        console.log(ImagePicker.launchImageLibrary);
+        let mediaType: ImagePicker.MediaType = "photo";
+        const options = {
+            mediaType: mediaType
+        }
+        const result = await ImagePicker.launchImageLibrary(options, () => {
+            console.log(123);
+        });
+        // ImagePicker.launchCamera(options, (response) => {
+    
+        //   console.log('Response = ', response);
+        //   if (response.didCancel) {
+        //     console.log('User cancelled image picker');
+        //   } else if (response.error) {
+        //     console.log('ImagePicker Error: ', response.error);
+        //   } else {
+        //     const uri = response.uri;
+        //     const type = response.type;
+        //     const name = response.fileName;
+        //     const source = {
+        //       uri,
+        //       type,
+        //       name,
+        //     }
+        //     console.log("Source: " + source);
+        //   }
+        // });
+      }
     return (
         <>
-        {   props.images.length > 0 
-            ? <View>
-                <Carousel
+        {   images.length > 0 
+            ? <Carousel
                 loop
                 width={width}
                 height={width/ 1.5}
                 autoPlay={false}
-                data={props.images}
+                data={images}
                 scrollAnimationDuration={1000}
                 onSnapToItem={(index) => console.log('current index:', index)}
                 renderItem={({ index, item }) => ( 
                     <RNImage source={{ uri: item.url }} style={{ width: width, height: width / 1.5 }} /> 
                 )}
             />
-            </View>
         : <View className='flex justify-center items-center w-full h-1/3 bg-gray-500'> 
             <Text className='text-white'>No images</Text> 
+            <Button onPress={() => selectPhotoTapped()} className="bg-white">
+            <Ionicons name='camera-outline' size={20} color="black" />
+            </Button>
           </View>
         }
-        
-        <AddPictureButton showAddPictureButton={showAddPictureButton} />
+
         </>
     )
 }
